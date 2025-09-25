219eea82e8dae5af07e792aadf224f9b7dec46ae
sohan-itobuz <sohan@itobuz.com>
2025-09-25 19:14:57 +0530

    initial get request testing

--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
-function fun(a, b) {
-  console.log(a + b);
-}
-let a = 6;
-let b = 7;
-fun(a, b);
+import express from 'express'
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  const status = {
+    Status: 'Running',
+    message: 'Server is up and running',
+  }
+  res.send(status)
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
